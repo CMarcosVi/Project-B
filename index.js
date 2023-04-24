943a9fe0a75fe0e4140147735a9283ca291e1ec9
Marcos <127824847+CMarcosVi@users.noreply.github.com>
2023-04-24 05:40:46 -0300

    v0.3

--- index.js
+++ index.js
@@ -4,8 +4,31 @@ function typeWrite(elemento){
     textArray.forEach((letra, i) => {
         setTimeout(() => {
             elemento.innerHTML += letra
-        }, 75*i);
+        }, 25*i);
     });
 }
 const titulo = document.querySelector('.TituloGame')
+const carta = document.querySelector('.historiaDojogo')
 typeWrite(titulo)
+typeWrite(carta)
+
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+    if(sections.length) {
+      const windowMetade = window.innerHeight * 0.7;
+  
+      function animaScroll() {
+        sections.forEach((section) => {
+          const sectionTop = section.getBoundingClientRect().top;
+          const isSectionVisible = (sectionTop - windowMetade) < 0;
+          if(isSectionVisible)
+            section.classList.add('ativo');
+        })
+      }
+  
+      animaScroll();
+  
+      window.addEventListener('scroll', animaScroll);
+    }
+  }
+  initAnimacaoScroll();
